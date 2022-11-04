b283647e51bfee3c9e9f4bbd2f63f65ae0524596
库里 <shuaiyu.t@aistarfish.com>
2022-11-04 10:30:31 +0800

    feat(*):上传magic-api项目

---
+++ bloom-spring-boot-lego/spring-boot-magic-api/src/main/java/com/bloom/springbootmagicapi/SpringBootMagicApiApplication.java
@@ -0,0 +1,13 @@
+package com.bloom.springbootmagicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMagicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMagicApiApplication.class, args);
+	}
+
+}
---
+++ bloom-spring-boot-lego/spring-boot-magic-api/src/test/java/com/bloom/springbootmagicapi/SpringBootMagicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloom.springbootmagicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMagicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
