c001004d69aa5c542dd67b98526070d8b34a96c1
库里 <shuaiyu.t@aistarfish.com>
2022-11-07 18:19:52 +0800

    feat(*):上传magic-api项目

---
+++ bloom-spring-boot-lego/spring-boot-magic-api/src/main/java/com/bloom/springbootmagicapi/service/UserService.java
@@ -0,0 +1,22 @@
+package com.bloom.springbootmagicapi.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author curry
+ * Created by on 2022-11-07 下午4:42
+ */
+@Service(value = "userService")
+public class UserService {
+
+    public List<String> userList() {
+        List<String> list = new ArrayList<>();
+        list.add("库里");
+        list.add("汤普森");
+        list.add("格林");
+        return list;
+    }
+}
