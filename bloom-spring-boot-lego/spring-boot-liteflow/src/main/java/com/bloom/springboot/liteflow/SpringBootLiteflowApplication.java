b76fc5002d7aa9b37de24ad05e704b37fa2926e3
库里 <shuaiyu.t@aistarfish.com>
2023-03-24 16:14:48 +0800

    🐱feat(*):集成liteflow规则引擎🌶

---
+++ bloom-spring-boot-lego/spring-boot-liteflow/src/main/java/com/bloom/springboot/liteflow/SpringBootLiteflowApplication.java
@@ -0,0 +1,13 @@
+package com.bloom.springboot.liteflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLiteflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLiteflowApplication.class, args);
+    }
+
+}
---
+++ bloom-spring-boot-lego/spring-boot-liteflow/src/test/java/com/bloom/springboot/liteflow/SpringBootLiteflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloom.springboot.liteflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLiteflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
